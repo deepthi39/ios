e69b771e718419e1f29bdd21a470527c9e1b6f8e
Deepthi Kaithi <89281610+deepthi39@users.noreply.github.com>
2022-02-08 21:21:26 -0600

    Assignment 3

---
+++ Kaithi_Calculator/Kaithi_Calculator/ViewController.swift
@@ -0,0 +1,99 @@
+//
+//  ViewController.swift
+//  Kaithi_Calculator
+//
+//  Created by Kaithi,Deepthi on 2/8/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    
+    @IBOutlet weak var displayLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    
+    @IBAction func ACbutton(_ sender: UIButton) {
+    }
+    
+    
+    @IBAction func Cbutton(_ sender: UIButton) {
+    }
+    
+    
+    @IBAction func comboButton(_ sender: UIButton) {
+    }
+    
+    
+    @IBAction func divButton(_ sender: UIButton) {
+    }
+    
+    
+    @IBAction func button7(_ sender: UIButton) {
+    }
+   
+    
+    @IBAction func button8(_ sender: UIButton) {
+    }
+    
+    
+    @IBAction func button9(_ sender: UIButton) {
+    }
+    
+    @IBAction func mulButton(_ sender: UIButton) {
+    }
+    
+    @IBAction func button4(_ sender: UIButton) {
+    }
+    
+    
+    @IBAction func button5(_ sender: UIButton) {
+    }
+    
+    
+    @IBAction func button6(_ sender: UIButton) {
+    }
+    
+
+    @IBAction func minusButton(_ sender: UIButton) {
+    }
+    
+    
+    @IBAction func button1(_ sender: UIButton) {
+    }
+    
+    
+    @IBAction func button2(_ sender: UIButton) {
+    }
+    
+    
+    @IBAction func button3(_ sender: UIButton) {
+    }
+    
+    
+    @IBAction func plusButton(_ sender: UIButton) {
+    }
+    
+    
+    @IBAction func button0(_ sender: UIButton) {
+    }
+    
+    
+    @IBAction func dotButton(_ sender: UIButton) {
+    }
+    
+    
+    @IBAction func modButton(_ sender: UIButton) {
+    }
+    
+    
+    @IBAction func equalButton(_ sender: UIButton) {
+    }
+    
+}
+
